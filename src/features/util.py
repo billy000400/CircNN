6381f264aa58c0695bcc55df5c7f2b9cfab6e5ea
Billy Li <li000400@umn.edu>
2022-06-05 17:06:39 -0500

    added a arc fitting function

--- src/features/util.py
+++ src/features/util.py
@@ -44,3 +44,41 @@ def plot_in_RAM(hit_dict, resolution):
     x = np.array(canvas.renderer.buffer_rgba())
     x = np.rint(x[...,:3] @ [0.2126, 0.7152, 0.0722]).astype(np.uint8)
     return np.array([x/255.0], dtype=np.float32)
+
+def hits2arc(hit_dict):
+    print("Arc Fitting")
+    hits = [ it for k, it in hit_dict.items()]
+    xs = []
+    ys = []
+    for hit in hits:
+        xs.append(hit[0])
+        ys.append(hit[1])
+    xs = np.array(xs)
+    ys = np.array(ys)
+    n = xs.size
+
+    a = 0
+    b = 0
+    R = 100
+
+    # https://math.stackexchange.com/questions/2898295/how-to-quickly-fit-a-circle-by-given-random-arc-points
+    print("assembling vec")
+    vec = [np.sum(xs*xs*xs)+np.sum(xs*ys*ys),\
+            np.sum(ys*ys*ys)+np.sum(xs*xs*ys),\
+            np.sum(xs*xs)+np.sum(ys*ys)]
+    vec = np.array(vec)
+    print(vec.shape)
+    print("assembling matrix")
+    matrix = [[2*np.sum(xs*xs), 2*np.sum(xs*ys), np.sum(xs)],\
+                [2*np.sum(xs*ys), 2*np.sum(ys*ys), np.sum(ys)],\
+                [2*np.sum(xs), 2*np.sum(ys), n]]
+    matrix = np.array(matrix)
+    matrix_inv = np.linalg.inv(matrix)
+    print(matrix_inv.shape)
+    print("multiplication")
+    [a, b, _] = matrix_inv.dot(vec)
+    print("calculating c")
+    c = (np.sum(xs*xs)+np.sum(ys*ys)-2*a*sum(xs)-2*b*sum(ys))/n
+    print("calculating R")
+    R = np.sqrt(c+a*a+b*b)
+    return a, b, R
